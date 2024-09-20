c922e6d7310e2b4377ddc699bbfbb8b1903fe13f
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-09-20 16:30:20 +0600

    product schame model done

---
+++ src/models/product.model.js
@@ -0,0 +1,24 @@
+import mongoose , {Schema} from 'mongoose'
+
+const productSchema = new Schema({
+    productName : {
+        type : String,
+        required : true,
+        trim : true,
+    },
+    productDescription : {
+        type : String,
+        required : true,
+        trim : true,
+    },
+    price : {
+        type : Number,
+        required : true
+    },
+    stockQuantity : {
+        type : Number,
+        default : 0
+    }
+},{
+    timestamps : true
+})
